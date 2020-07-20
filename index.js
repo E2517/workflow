d7e4ad004612bc73410633f47083234762b541d2
E2517 <61824329+E2517@users.noreply.github.com>
2020-07-20 12:39:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let { EMAIL_GMAIL } = process.env;
 
 let password = `${EMAIL_GMAIL}`;
 
-if (password === "123abc") {
+if (password === "ccrlawyers@gmail.com") {
   fs.writeFile("prueba.txt", "Hello world!", function (err) {
     if (err) throw err;
     console.log("Saved!");
